fdaf65780723516af118437ad954a6a9a9be7bdf
티맵모빌리티 장영연 <yeongyeon.jang@sk.com>
2021-08-09 01:21:28 +0900

    commit

---
+++ src/test/java/com/yom/blinddate/BlinddateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yom.blinddate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlinddateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

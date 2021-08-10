a579f6567ca573b14bf610f9416953d4fef5ca20
youngsmith <yyjjang9@naver.com>
2021-08-11 03:58:23 +0900

    commit

--- src/main/java/com/yom/blinddate/exception/GlobalExceptionHandler.java
+++ src/main/java/com/yom/blinddate/exception/GlobalExceptionHandler.java
@@ -5,13 +5,17 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Arrays;
-
+/*
+https://spring.io/blog/2013/11/01/exception-handling-in-spring-mvc
+https://www.baeldung.com/exception-handling-for-rest-with-spring
+https://supawer0728.github.io/2019/04/04/spring-error-handling/
+https://www.baeldung.com/global-error-handler-in-a-spring-rest-api
+ */
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(ArithmeticException.class)
     public ResponseEntity<String> handleConflict(RuntimeException ex, HttpServletRequest request) {
-        System.out.println(Arrays.toString(ex.getStackTrace()));
+        System.out.println(ex.getClass());
         System.out.println(request.getRequestURI());
         return ResponseEntity.badRequest().body("exception handler error!");
     }

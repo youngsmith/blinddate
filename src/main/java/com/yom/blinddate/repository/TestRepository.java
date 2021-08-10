87a2d5aeb836ac5462874427cfd18ec71d90a2b6
티맵모빌리티 장영연 <yeongyeon.jang@sk.com>
2021-08-11 02:56:04 +0900

    commit

--- src/main/java/com/yom/blinddate/repository/TestRepository.java
+++ src/main/java/com/yom/blinddate/repository/TestRepository.java
@@ -1,11 +1,9 @@
 package com.yom.blinddate.repository;
 
 import com.yom.blinddate.model.TestModel;
-import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
-@Mapper
 public interface TestRepository {
     List<TestModel> selectAll();
 }
--- src/main/java/com/yom/blinddate/repository/TestTwoRepository.java
+++ src/main/java/com/yom/blinddate/repository/TestTwoRepository.java
@@ -1,8 +1,5 @@
 package com.yom.blinddate.repository;
 
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
 public interface TestTwoRepository {
     boolean isExist();
 }
